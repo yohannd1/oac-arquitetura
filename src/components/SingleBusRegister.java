a26ff6242552e54cb9ddd7e04449ed561967fedc
yohannd1 <mitonanan12@gmail.com>
2025-07-01 16:13:32 -0300

    components.SingleBusRegister: classe nova para registradores de um barramento só

---
+++ src/components/SingleBusRegister.java
@@ -0,0 +1,46 @@
+package components;
+
+public class SingleBusRegister {
+	private String name;
+	private int data;
+	private Bus bus;
+
+	public SingleBusRegister(String name, Bus bus) {
+		this.name = name;
+		this.bus = bus;
+	}
+
+	/**
+	 * Get the register's value. Should not be used in
+	 * microprograms.
+	 */
+	public int getData() {
+		return data;
+	}
+
+	/**
+	 * Set the register's value. Should not be used in
+	 * microprograms.
+	 */
+	public int setData(int x) {
+		data = x;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Store the data from the bus into the register.
+	 */
+	public void store() {
+		data = bus.get();
+	}
+
+	/**
+	 * Read the data from the register into the bus.
+	 */
+	public void read() {
+		bus.put(data);
+	}
+}
