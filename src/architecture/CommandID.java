6140271ba47e3ace2b73abfa32090c6971573102
yohannd1 <mitonanan12@gmail.com>
2025-07-01 16:04:16 -0300

    architecture.CommandID: melhorando alinhamento dos comentários

--- src/architecture/CommandID.java
+++ src/architecture/CommandID.java
@@ -1,27 +1,27 @@
 package architecture;
 
 public enum CommandID {
-	ADD_REG_REG, // add %<regA> %<regB>
-	ADD_MEM_REG, // add <mem> %<regA>
-	ADD_REG_MEM, // add %<regA> <mem>
-	SUB_REG_REG, // sub <regA> <regB>
-	SUB_MEM_REG, // sub <mem> %<regA>
-	SUB_REG_MEM, // sub %<regA> <mem>
+	ADD_REG_REG,  // add %<regA> %<regB>
+	ADD_MEM_REG,  // add <mem> %<regA>
+	ADD_REG_MEM,  // add %<regA> <mem>
+	SUB_REG_REG,  // sub <regA> <regB>
+	SUB_MEM_REG,  // sub <mem> %<regA>
+	SUB_REG_MEM,  // sub %<regA> <mem>
 	MOVE_MEM_REG, // move <mem> %<regA>
 	MOVE_REG_MEM, // move %<regA> <mem>
 	MOVE_REG_REG, // move %<regA> %<regB>
 	MOVE_IMM_REG, // move imm %<regA>
-	INC_REG, // inc %<regA>
-	INC_MEM, // inc <mem>
-	JMP, // jmp <mem>
-	JN, // jn <mem>
-	JZ, // jz <mem>
-	JNZ, // jnz <mem>
-	JEQ, // jeq %<regA> %<regB> <mem>
-	JGT, // jgt %<regA> %<regB> <mem>
-	JLW, // jlw %<regA> %<regB> <mem>
-	CALL, // call <mem>
-	RET; // ret
+	INC_REG,      // inc %<regA>
+	INC_MEM,      // inc <mem>
+	JMP,          // jmp <mem>
+	JN,           // jn <mem>
+	JZ,           // jz <mem>
+	JNZ,          // jnz <mem>
+	JEQ,          // jeq %<regA> %<regB> <mem>
+	JGT,          // jgt %<regA> %<regB> <mem>
+	JLW,          // jlw %<regA> %<regB> <mem>
+	CALL,         // call <mem>
+	RET;          // ret
 
 	static private CommandID[] variants = CommandID.values();
 
