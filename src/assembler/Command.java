9767b669140bf2236c564d086c9f512c15490628
yohannd1 <mitonanan12@gmail.com>
2025-07-01 01:40:24 -0300

    assembler.Command: melhorando toString()

--- src/assembler/Command.java
+++ src/assembler/Command.java
@@ -11,8 +11,23 @@ public class Command {
 		this.args = args;
 	}
 
+	private static <T> String arrayToString(T[] arr) {
+		if (arr.length == 0)
+			return "[]";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (int i = 0; i < arr.length - 1; i++) {
+			sb.append(arr[i].toString());
+			sb.append(", ");
+		}
+		sb.append(arr[arr.length - 1].toString());
+		sb.append("]");
+		return sb.toString();
+	}
+
 	@Override
 	public String toString() {
-		return String.format("Command[id=%s, args=%s]", id, args);
+		return String.format("Command[id=(%s, code=%d), args=%s]", id, id.toInt(), Command.arrayToString(args));
 	}
 }
