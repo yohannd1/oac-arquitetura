c777b56dbed9ee271e46e4adab33018159060127
yohannd1 <mitonanan12@gmail.com>
2025-07-02 15:06:09 -0300

    assembler.Assembler.Command: melhorando print de debug

--- src/assembler/Assembler.java
+++ src/assembler/Assembler.java
@@ -483,7 +483,22 @@ public class Assembler {
 
 		@Override
 		public String toString() {
-			return String.format("Command[id=%s, args=%s]", id, args);
+			return String.format("Command[id=%s, args=%s]", id, arrayToString(args));
 		}
+	}
+
+	private static <T> String arrayToString(T[] arr) {
+		if (arr.length == 0)
+			return "[]";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (int i = 0; i < arr.length - 1; i++) {
+			sb.append(arr[i].toString());
+			sb.append(", ");
+		}
+		sb.append(arr[arr.length - 1].toString());
+		sb.append("]");
+		return sb.toString();
 	}
 }
