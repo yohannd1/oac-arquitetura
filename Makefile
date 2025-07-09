4aec63aefbc828f74d9347f30bc2c0bee2ba4856
yohannd1 <mitonanan12@gmail.com>
2025-07-08 22:29:05 -0300

    melhorando sistema de build e adicionando suporte a testes (com JUnit)

--- Makefile
+++ Makefile
@@ -1,20 +1,13 @@
-SRC_DIR := src
-BUILD_DIR := bin
-CFLAGS := -Xlint:unchecked
-
-SOURCES := $(wildcard $(SRC_DIR)/**/*.java)
-PROGRAM := $(BUILD_DIR)/built.txt
-
-$(PROGRAM): $(SOURCES)
-	mkdir -p $(BUILD_DIR)
-	javac -d $(BUILD_DIR) $(CFLAGS) $(SOURCES)
-	touch $(PROGRAM)
+build:
+	./build.bash build
 
 clean:
-	if [ -r $(BUILD_DIR) ]; then rm -r $(BUILD_DIR); fi
+	./build.bash clean
 
-run: $(PROGRAM)
-	java -cp $(BUILD_DIR) assembler.Assembler program
-	java -cp $(BUILD_DIR) architecture.Architecture program
+run:
+	./build.bash run
 
-.PHONY: clean run
+test:
+	./build.bash test
+
+.PHONY: clean run build test
---
+++ build.bash
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+srcDir="src"
+buildDir="bin"
+depsDir="deps"
+
+set -ue
+progname=$(basename "$0")
+
+showHelp() {
+  printf >&2 "Usage: %s { build | run | test | clean }\n" "$progname"
+  exit 2
+}
+
+[ $# = 1 ] || showHelp
+
+makeClassPath() {
+  printf "%s:" "$buildDir" "$depsDir"/*.jar
+  printf "."
+}
+
+mkdir -p "$buildDir"
+mkdir -p "$depsDir"
+
+run() {
+  printf >&2 "%s\n" "$*"
+  "$@"
+}
+
+downloadIfNotExist() {
+  [ $# = 2 ] || exit 2
+
+  if [ ! -e "$1" ]; then
+    curl -L --output "$1" -- "$2"
+  fi
+}
+
+doBuild() {
+  sources=("$srcDir"/**/*.java)
+  cflags=(-Xlint:unchecked)
+
+  downloadIfNotExist "$depsDir/junit.jar" \
+    "https://repo1.maven.org/maven2/junit/junit/4.13.2/junit-4.13.2.jar"
+
+  downloadIfNotExist "$depsDir/junit-platform-console-standalone.jar" \
+    "https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.13.3/junit-platform-console-standalone-1.13.3.jar"
+
+  downloadIfNotExist "$depsDir/junit-jupiter-engine.0.0-M1.jar" \
+    "https://repo1.maven.org/maven2/org/junit/jupiter/junit-jupiter-engine/6.0.0-M1/junit-jupiter-engine-6.0.0-M1.jar"
+
+  downloadIfNotExist "$depsDir/junit-jupiter-api.0.0-M1.jar" \
+    "https://repo1.maven.org/maven2/org/junit/jupiter/junit-jupiter-api/6.0.0-M1/junit-jupiter-api-6.0.0-M1.jar"
+
+  classPath=$(makeClassPath)
+  run javac -d "$buildDir" -cp "$classPath" "${cflags[@]}" "${sources[@]}"
+}
+
+doClean() {
+  if [ -r "$buildDir" ]; then rm -r "$buildDir"; fi
+  if [ -r "$depsDir" ]; then rm -r "$depsDir"; fi
+}
+
+doRun() {
+  doBuild
+  classPath=$(makeClassPath)
+
+  run java -cp "$classPath" assembler.Assembler program
+  run java -cp "$classPath" architecture.Architecture program
+}
+
+doTest() {
+  doBuild
+
+  run java -cp "$classPath" org.junit.platform.console.ConsoleLauncher execute --scan-classpath
+}
+
+case "$1" in
+  build) doBuild ;;
+  clean) doClean ;;
+  run) doRun ;;
+  test) doTest ;;
+  *) showHelp ;;
+esac
